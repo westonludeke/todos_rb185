9ebed289a7a4641a528197aaa6f74e9dcb3b84b2
westonludeke <westonludeke@gmail.com>
2021-11-07 14:49:02 -0600

    fixed bug with mark_all_todos_as_completed method

--- database_persistence.rb
+++ database_persistence.rb
@@ -78,7 +78,7 @@ class DatabasePersistence
   end
 
   def mark_all_todos_as_completed(list_id)
-    sql = "UPDATE todos SET completed = true WHERE list_id = 1"
+    sql = "UPDATE todos SET completed = true WHERE list_id = $1"
     query(sql, list_id)
   end
 
